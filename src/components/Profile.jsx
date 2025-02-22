411dea3ce38f86107563b5fe7297d6d181c109cb
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-23 01:03:17 +0300

    Profile.jsx created as a static

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,6 +2,44 @@ import React from 'react'
 
 export default function Profile() {
     return (
-        <div>Profile</div>
+        <>
+            <div className='text-[40px]'>Profile</div>
+
+
+            <div className='flex justify-between ' >
+                <div>
+
+                    <div className='text-[#4338CA]'>Profile</div>
+                    <div className='flex flex-col'>
+                        <div className='flex'>
+                            <div>Dogum Tarihi</div>
+                            <p>29.04.1996</p>
+                        </div>
+                        <div className='flex'>
+                            <div>Ikamet Sehri</div>
+                            <p>Ankara</p>
+                        </div>
+                        <div className='flex'>
+                            <div>Egitim Durumu</div>
+                            <p>Baskent Universitesi</p>
+                        </div>
+
+                        <div className='flex'>
+                            <div>Tercih Ettigi rol</div>
+                            <p>Front-End/UI</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='w-[50%]'>
+                    <div className='text-[#4338CA]'>
+                        About me
+                    </div>
+                    <p className='text-gray-400'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.
+                        Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam! </p>
+                </div>
+            </div>
+
+        </>
     )
 }
