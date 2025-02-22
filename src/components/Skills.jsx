d842c7a9b6be68215b6bac66f34d87ff67202c85
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-23 00:44:16 +0300

    Skills.jsx created as a static

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default function Skills() {
     return (
-        <div>Skills</div>
+        <>
+
+            <div className="text-[40px]">Skills</div>
+
+            <div className='flex'>
+                <div>
+                    <div className='text-[#4338CA]'>Java Script</div>
+                    <p className='text-gray-400'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div>
+                    <div className='text-[#4338CA]' >React.Js</div>
+                    <p className='text-gray-400'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <div>
+                    <div className='text-[#4338CA]'>Node.Js</div>
+                    <p className='text-gray-400'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                </div>
+
+
+            </div>
+
+
+
+        </>
     )
 }
