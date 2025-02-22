a3f5c9fb6fe63f8c1b39bfebf9f7a8a297379285
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-23 01:25:07 +0300

    Footer.jsx created as a static

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,22 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <>
+            <div>
+                <div>Lets work together on</div>
+                <div>your next product.</div>
+            </div>
+            <div className='flex justify-between'>
+                <a href="m.doganguzell@gmail.com">👉 m.doganguzell@gmail.com</a>
+                <div className='flex gap-4 '>
+                    <a href="">Personal Blog </a>
+                    <a href="">Github</a>
+                    <a href="">Linkedin</a>
+                </div>
+
+            </div>
+
+
+        </>
     )
 }
