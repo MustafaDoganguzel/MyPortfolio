6a227f49e7f098a3598a7c5970fa893aedbaf407
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-23 00:32:04 +0300

    header.jsx created as a static

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Header() {
     return (
         <>
-            <div className='header flex justify-between px-5 py-5'>
+            <div className='header flex justify-between px-5 py-5 '>
                 <div className='rounded-full w-10 h-10 flex items-center justify-center bg-[#EEEBFF]'> <span className=' text-[#7B61FF] inline-block rotate-20'>M</span> </div>
                 <div className='flex gap-2'>
                     <button className='border flex items-center px-1 rounded-lg hover:bg-[#3730A3] hover:text-white'>Skills
