7b5d0c3180eca01dd776aa74c77cd3738723e676
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-23 00:32:24 +0300

    headerButton.jsx created as a static

--- src/components/HeaderButton.jsx
+++ src/components/HeaderButton.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function HeaderButton() {
     return (
         <>
-            <div className='nav flex justify-end pt-5 pr-5'>
+            <div className='nav flex justify-end pt-5 pr-5 '>
                 <div>Dark Mode</div>
                 <div> Turkceye Gec</div>
             </div >
