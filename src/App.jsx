caeffb0777ca23f59cec61c1308aaff052420623
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-22 17:37:37 +0300

    restructure components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import HeaderButton from './components/HeaderButton'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Skills from './components/Skills'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+import Profile from './components/Profile.Jsx'
+
 import './App.css'
 
+
+
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div className='bg-red-500'>deneme</div>
+      <HeaderButton />
+      <Header />
+      <Hero />
+      <Skills />
+      <Profile />
+      <Projects />
+      <Footer />
     </>
   )
 }
