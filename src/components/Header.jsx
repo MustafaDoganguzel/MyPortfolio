e54ae6362455bc80d87104030a26c19e00ef270b
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-24 01:37:49 +0300

    Header.jsx revised as dynamic
    Dynamic Header Component: Added header component generated from API data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,16 +4,16 @@ import React from 'react'
 export default function Header() {
     return (
         <>
-            <div className='header flex justify-between px-5 py-5 '>
+            <div className='header flex justify-between px-5 py-5'>
                 <div className='rounded-full w-10 h-10 flex items-center justify-center bg-[#EEEBFF]'> <span className=' text-[#7B61FF] inline-block rotate-20'>M</span> </div>
                 <div className='flex gap-2'>
-                    <button className=' flex items-center px-1 rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer '>Skills
+                    <button className='font-semibold flex items-center px-10 rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer '>Skills
                     </button>
 
-                    <button className='  flex items-center px-1 rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer'>Projects
+                    <button className='font-semibold  flex items-center px-10  rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer'>Projects
                     </button>
 
-                    <button className=' text-[#3730A3] border flex items-center px-1 rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer'>Hire Me
+                    <button className=' font-semibold text-[#3730A3] bg-[#FFFFFF]  flex items-center px-10 rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer'>Hire Me
                     </button>
                 </div>
             </div>
