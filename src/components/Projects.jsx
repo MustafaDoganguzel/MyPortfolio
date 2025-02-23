0a16b14009f67b5e476b1f24266f7ea236791d9c
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-24 02:32:17 +0300

    Projects.jsx revised as dynamic

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-export default function Projects() {
+export default function Projects({ projectsData }) {
     return (
         <div className='projects '>
 
-            <div className='text-[40px]'>Projects</div>
+            <div className='text-[40px]'>{projectsData?.title}</div>
             <div className='flex m-5 justify-between'>
 
-                <div className='w-50'>
-                    <img className='w-50 h-35' src="src/images/Pizza.jpg" alt="pizza-img" />
-                    <div className='text-[#4338CA] text-xl '>Pizza</div>
-                    <p className='text-xs text-[#6B7280]'>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. </p>
+                <div className='w-80'>
+                    <img className='w-80 h-35' src="src/images/Pizza.jpg" alt="pizza-img" />
+                    <div className='text-[#4338CA] text-xl '>{projectsData?.project1.title}</div>
+                    <p className='h-35 text-xs text-[#6B7280]'>{projectsData?.project1.description}</p>
                     <div className='flex gap-1 pt-1'>
                         <button className='border text-[#3730A3] text-sm rounded-md px-1'> React</button>
                         <button className='border text-[#3730A3] text-sm rounded-md px-1'>Redux</button>
@@ -18,14 +18,14 @@ export default function Projects() {
                     </div>
                     <div className='flex justify-between pt-1 text-[#3730A3] text-sm underline'>
                         <a href="">Github</a>
-                        <a href="">View Site</a>
+                        <a href="">{projectsData?.viewSite}</a>
                     </div>
                 </div>
 
-                <div className='w-50'>
-                    <img className='w-50 h-35' src="src/images/Random.png" alt="random-png" />
-                    <div className='text-[#4338CA] text-xl '>Witflix</div>
-                    <p className='text-xs text-[#6B7280]'>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. </p>
+                <div className='w-80'>
+                    <img className='w-80 h-35' src="src/images/Random.png" alt="random-png" />
+                    <div className='text-[#4338CA] text-xl '>{projectsData?.project2.title}</div>
+                    <p className='h-35 text-xs text-[#6B7280]'>{projectsData?.project2.description}</p>
                     <div className='flex gap-1 pt-1 '>
                         <button className='border text-[#3730A3] text-sm rounded-md px-1'> React</button>
                         <button className='border text-[#3730A3] text-sm rounded-md px-1'>Redux</button>
@@ -33,14 +33,14 @@ export default function Projects() {
                     </div>
                     <div className='flex justify-between pt-1 text-[#3730A3] text-sm underline'>
                         <a href="">Github</a>
-                        <a href="">View Site</a>
+                        <a href="">{projectsData?.viewSite}</a>
                     </div>
                 </div>
 
-                <div className='w-50'>
-                    <img className='w-50 h-35' src="src/images/Workintech.png" alt="workintech-png" />
-                    <div className='text-[#4338CA] text-xl '>Random Jokes</div>
-                    <p className='text-xs text-[#6B7280]'>A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. </p>
+                <div className='w-80'>
+                    <img className='w-80 h-35' src="src/images/Workintech.png" alt="workintech-png" />
+                    <div className='text-[#4338CA] text-xl '>{projectsData?.project3.title}</div>
+                    <p className='h-35 text-xs text-[#6B7280]'>{projectsData?.project3.description}</p>
                     <div className='flex gap-1 pt-1'>
                         <button className='border text-[#3730A3] text-sm rounded-md px-1'> React</button>
                         <button className='border text-[#3730A3] text-sm rounded-md px-1'>Redux</button>
@@ -48,7 +48,7 @@ export default function Projects() {
                     </div>
                     <div className='flex justify-between pt-1 text-[#3730A3] text-sm underline '>
                         <a href="">Github</a>
-                        <a href="">View Site</a>
+                        <a href="">{projectsData?.viewSite}</a>
                     </div>
                 </div>
 
