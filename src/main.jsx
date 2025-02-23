e5941ba438156cbfba0c341475da2684e58dc07b
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-24 01:40:34 +0300

    ContextProviders implemented into the Projects

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LanguageContextProvider from './contexts/LanguageContext.jsx'
+import ThemeModeProvider from './contexts/ThemeMode.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <LanguageContextProvider>
+    <ThemeModeProvider>
+      <App />
+    </ThemeModeProvider>
+  </LanguageContextProvider>
+
+
 )
