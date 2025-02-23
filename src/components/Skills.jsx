506655279b9243734d7ed3a3f4171dab35733697
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-24 01:41:28 +0300

    Skills.jsx revised as dynamic

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,23 +1,31 @@
 import React from 'react'
 
-export default function Skills() {
+export default function Skills({ skillsData }) {
     return (
         <div className='skills'>
 
-            <div className="text-[40px]">Skills</div>
+            <div className="text-[40px]"> {skillsData?.title}</div>
 
-            <div className='flex m-5'>
+            <div className='flex justify-between my-5'>
                 <div>
-                    <div className='text-[#4338CA]'>Java Script</div>
-                    <p className='text-gray-400'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div className='text-[#4338CA]'>
+                        {skillsData?.javascript.name}
+                    </div>
+                    <p className='text-gray-400 w-100 h-45 '>
+                        {skillsData?.javascript.info}
+                    </p>
                 </div>
                 <div>
-                    <div className='text-[#4338CA]' >React.Js</div>
-                    <p className='text-gray-400'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div className='text-[#4338CA]' >  {skillsData?.react.name}</div>
+                    <p className='text-gray-400 w-90 h-45  '>
+                        {skillsData?.react.info}
+                    </p>
                 </div>
                 <div>
-                    <div className='text-[#4338CA]'>Node.Js</div>
-                    <p className='text-gray-400'>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <div className='text-[#4338CA]'>
+                        {skillsData?.node.name}
+                    </div>
+                    <p className='text-gray-400 w-90 h-45 '>  {skillsData?.node.info}</p>
 
                 </div>
 
