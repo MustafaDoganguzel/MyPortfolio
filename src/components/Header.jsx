a1ee175ad0250570dd471fcdc6dd73148bf5bdb1
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-02-25 11:53:48 +0300

    scroll Into view example implemented

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 
 
 export default function Header() {
+    const scrollSkills = () => {
+        const skillsSection = document.getElementById("skills-section")
+        if (skillsSection) {
+            skillsSection.scrollIntoView({ behavior: "smooth" })
+        }
+    }
+    //devam ettir burayi
     return (
         <>
-            <div className='header flex justify-between px-5 py-5'>
+            <div className=' header flex justify-between px-5 py-5'>
                 <div className='rounded-full w-10 h-10 flex items-center justify-center bg-[#EEEBFF]'> <span className=' text-[#7B61FF] inline-block rotate-20'>M</span> </div>
                 <div className='flex gap-2'>
-                    <button className='font-semibold flex items-center px-10 rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer '>Skills
+                    <button onClick={scrollSkills} className='font-semibold flex items-center px-10 rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer '>Skills
                     </button>
 
                     <button className='font-semibold  flex items-center px-10  rounded-lg hover:bg-[#3730A3] hover:text-white hover:cursor-pointer'>Projects
